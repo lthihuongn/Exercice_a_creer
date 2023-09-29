626043eea4e1e7756dcde773078d09d246fbf3ab
Lcjim <diamxridot@gmail.com>
2023-09-29 09:43:51 +0200

    F

---
+++ brouillon calcul addition binaire.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int CalculBinaire(int nombreB){
+    int somme=0;
+    int tabB[4]={};
+    int
+    for (int i = 0;i;++i){
+        if (tabB[i] = 0 && tabB[i+1]=1){
+
+        }
+        else if (tabB[i]= 1 && tabB[i+1]= 1){
+            somme=0;
+            tabB[i+1]=1;
+            return somme, tabB[i];
+        }
+    }
+}
+int main(){
+    int somme=0;
+    int nombreB;
+    printf("donner un nombre en binaire \n");
+    scantf("%d",&nombreB);
+    getchar();
+
+}
